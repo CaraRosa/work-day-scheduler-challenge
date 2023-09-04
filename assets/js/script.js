192e9852146330f11da36197ef187b6dc4f53e78
Cara R <cara.rosa.02@gmail.com>
2023-09-04 12:05:27 -0500

    added current day, month, and day of month

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// var buttonEl = document.querySelector("#hour-9");
+var currentDay = document.querySelector("#currentDay");
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,6 +11,13 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  // document.getElementById("hour-9").addEventListener(click, saveButton);
+
+  // function saveButton(this) {
+
+  // }
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +29,8 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  const currentDate = dayjs();
+  const formattedDate = currentDate.format('dddd, MMMM D');
+  console.log(formattedDate);
+  document.querySelector('#currentDay').textContent = formattedDate;
 });
